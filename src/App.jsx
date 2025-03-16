2aa345758d559a085b2fed7aee6d476429a854fa
Prakhar Kabra <prakhar.rock09@gmail.com>
2025-03-16 22:30:40 +0530

    add daisyUI for enhanced styling; update App component with a button

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 export default function App() {
   return (
-    <h1 className="text-red-500 text-3xl font-bold underline">
-      Hello world!
+    <><h1 className="text-red-500 text-3xl font-bold underline">
+      Hello world!  
     </h1>
+    <button className="btn">Default</button>
+    </>
   )
 }
