292a2a85553726c2ef2ef60d26bb50121fc5fe97
Prakhar Kabra <prakhar.rock09@gmail.com>
2025-03-17 01:44:00 +0530

    implement routing with React Router; add Navbar and page components for Home, Login, SignUp, Settings, and Profile

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
-export default function App() {
+import Navbar from './components/Navbar'
+import {Routes, Route} from 'react-router-dom'
+import { HomePage, SignUpPage, LoginPage, SettingsPage, ProfilePage } from './pages'
+const App = () => {
   return (
-    <><h1 className="text-red-500 text-3xl font-bold underline">
-      Hello world!  
-    </h1>
-    <button className="btn">Default</button>
-    </>
+    <div>
+ 
+      <Navbar />
+
+      <Routes> 
+
+        {/* Routes component will render the appropriate component based on the URL */}
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        
+      </Routes>
+
+    </div>
+
   )
-}
+};
+
+export default App
