47b97530ac76412cc202128731b5794835a1018c
Prakhar Kabra <prakhar.rock09@gmail.com>
2025-03-17 02:01:41 +0530

    add React Router for navigation; implement Navbar and update App component with routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
+import React from 'react'
 import Navbar from './components/Navbar'
-import {Routes, Route} from 'react-router-dom'
-import { HomePage, SignUpPage, LoginPage, SettingsPage, ProfilePage } from './pages'
+import { Routes, Route } from 'react-router-dom'
+import HomePage from './pages/HomePage.jsx'
+import SignUpPage from './pages/SignUpPage'
+import LoginPage from './pages/LoginPage'
+import SettingsPage from './pages/SettingsPage'
+import ProfilePage from './pages/ProfilePage'
+
+
 const App = () => {
   return (
     <div>
@@ -9,18 +16,17 @@ const App = () => {
 
       <Routes> 
 
-        {/* Routes component will render the appropriate component based on the URL */}
         <Route path="/" element={<HomePage />} />
         <Route path="/signup" element={<SignUpPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/settings" element={<SettingsPage />} />
         <Route path="/profile" element={<ProfilePage />} />
-        
+
       </Routes>
 
     </div>
 
-  )
+  );
 };
 
-export default App
+export default App;
